73517b0d5454d8c9a652a674beb60ef570e54ead
Blake Loring <Blake.Loring@ig.com>
2014-12-24 10:55:19 +0000

    Docs

--- src/match.h
+++ src/match.h
@@ -4,6 +4,9 @@
 #include "nfa_list.h"
 #include <stdbool.h>
 
+/**
+ * Check if the target string matches with the NFA
+ */
 bool nfaMatches(nfa_state* nfa, char const* targetString);
 
 #endif //_NFA_SIMULATE_H_
